bc39a86046c89bbec6a148019949744ef1d9a1c9
114588-Ivetta Luis <114588@tecnicatura.frc.utn.edu.ar>
2022-07-05 22:20:56 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorMudule } from './contador/contador.module';
+
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
+
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HeroesModule,
+    ContadorMudule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
